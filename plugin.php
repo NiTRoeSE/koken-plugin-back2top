e8bdfa0f26c30d6ada1724c936dbba8c1ee442b6
NiTRoeSE <michael.schunk@online.de>
2017-01-07 00:06:30 +0100

    Fix Problem with .hide()

--- plugin.php
+++ plugin.php
@@ -44,7 +44,7 @@ class kokenback2top extends KokenPlugin {
             $(function () {
                 $(window).scroll(function () {
                     if ($(this).scrollTop() > 200) { /* if 200 pixel scrolled down, fade in button */
-                        $('.back2top_button').hide().fadeIn();
+                        $('.back2top_button').fadeIn();
                     } else {
                         $('.back2top_button').fadeOut();
                     }
